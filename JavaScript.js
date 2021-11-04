d36d2757866bb59854f6e83db6495be9743feaa2
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2021-11-04 17:57:44 +0330

    Ui created and fontawsome added.

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,8 @@
-
+const container = document.querySelector(".container");
+console.log(container);
+window.onmousemove = e=>{
+let x = e.clientX/30;
+let y = e.clientY/30;
+container.style.backgroundPositionX = x + "px";
+container.style.backgroundPositionY = y + "px";
+}
