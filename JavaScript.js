c74691c1f0d47a483396f7a791770fc50d1b0323
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2021-11-10 20:38:42 +0330

    Todo list donegit add .!

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,80 @@
-const container = document.querySelector(".container");
-console.log(container);
-window.onmousemove = e=>{
-let x = e.clientX/30;
-let y = e.clientY/30;
-container.style.backgroundPositionX = x + "px";
-container.style.backgroundPositionY = y + "px";
+const input = document.querySelector("#input");
+const addBtn = document.querySelector(".add-button");
+const ul = document.querySelector(".ul");
+const dropDown = document.querySelector("#dropdownlist")
+dropDown.addEventListener("click",dropDownfunc);
+
+addBtn.addEventListener("click" , addTask);
+
+
+
+
+function addTask(){
+    const eachTask = document.createElement("div");
+    eachTask.classList.add("eachTask");
+
+    const li = document.createElement("li");
+    li.innerText = input.value;
+
+    const checkAndTrashDiv = document.createElement("div");
+    checkAndTrashDiv.classList.add("checkandtrash");
+
+    const checkBtn = document.createElement("button");
+    checkBtn.classList.add("check");
+
+
+    const trashBtn = document.createElement("button");
+    trashBtn.classList.add("trash");
+
+    
+    const checkIcon = document.createElement("i");
+    checkIcon.classList.add("fas","fa-check");
+    checkBtn.append(checkIcon);
+    checkIcon.addEventListener("click",completeTask)
+    
+    const trashIcon = document.createElement("i");
+    trashIcon.classList.add("fas","fa-trash");
+    trashBtn.append(trashIcon)
+    trashIcon.addEventListener("click",deleteTask)
+
+    checkAndTrashDiv.append(checkBtn,trashBtn);
+    eachTask.append(li,checkAndTrashDiv);
+    ul.append(eachTask);
+    console.log(ul);
+    input.value = "";
+}
+
+function dropDownfunc(eve) {
+    tasks = ul.children;
+    console.log(tasks)
+    for (let item of tasks) {
+        switch (eve.target.value)
+        {
+       case "All":
+           item.style.display = "flex"
+           break;
+       case "Completed":
+            if(item.classList.contains("complete")){
+               item.style.display = "flex";
+            }else{
+               item.style.display = "none";
+            }
+           break;
+       case "Notcompleted":
+        if(item.classList.contains("complete")){
+           item.style.display = "none";
+        }else{
+           item.style.display = "flex";
+        }
+           break;
+    }}}
+    function deleteTask(eve){
+        const trashBtn = eve.target;
+        const parent = trashBtn.parentElement.parentElement.parentElement;
+        parent.remove();
+}
+function completeTask(eve) {
+    const completeBtn = eve.target;
+    const parent = completeBtn.parentElement.parentElement.parentElement;
+    parent.classList.toggle("complete")
 }
